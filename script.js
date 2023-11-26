be93deccfb81c2169c1da4be156f0f8a0c303bf2
Noor Choudhry <Noorchoudhry3@cmail.carleton.ca>
2023-11-25 20:45:14 -0500

    please be better i beg my eyes hurt

--- script.js
+++ script.js
@@ -4,46 +4,44 @@
  *
  */
 
-/* Assignment 04: Finishing a Todo List App */
-
 function addItem() {
-  var inputField = document.getElementById("todoInput");
-  var inputValue = inputField.value;
+  const inputField = document.getElementById("todoInput");
+  const inputValue = inputField.value;
 
+// an alert to type a task if the input field is empty
   if (inputValue.trim() === "") {
-      alert("Please enter a task!");
-      return;
+    alert("Please enter a task!");
+    return;
   }
 
-  var todoList = document.getElementById("todoList");
+  const todoList = document.getElementById("todoList");
 
-  // Create list item
-  var listItem = document.createElement("li");
+  // Creating the list item
+  const listItem = document.createElement("li");
   listItem.className = "todoItem";
 
-  // Create span for task text
-  var taskSpan = document.createElement("span");
-  taskSpan.textContent = inputValue;
+  // Creating a <p> for task text that input by user
+  const taskParagraph = document.createElement("p");
+  taskParagraph.textContent = inputValue;
 
-  // Create span for delete button
-  var deleteSpan = document.createElement("span");
-  deleteSpan.textContent = "Delete";
-  deleteSpan.className = "delete";
-  deleteSpan.onclick = function () {
-      todoList.removeChild(listItem);
+  // Creating button for delete 
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.className = "delete";
+  deleteButton.onclick = function () {
+    todoList.removeChild(listItem);
   };
 
-  // Append task text and delete button to list item
-  listItem.appendChild(taskSpan);
-  listItem.appendChild(deleteSpan);
+  // attach text and delete button together to list item
+  listItem.appendChild(taskParagraph);
+  listItem.appendChild(deleteButton);
 
-  // Append list item to todo list
+  // attach above to the list item to todo list
   todoList.appendChild(listItem);
 
-  // Clear input field
+  // boom clear input field
   inputField.value = "";
 }
-
 
 /* ------ ALREADY PUT IN STUFF ------ */
 //
@@ -80,43 +78,3 @@ console.log("App successfully initialised");
 // Inits & Event Listeners
 //
 inititialise();
-
-
-
-//
-// Variables
-//
-/*
-// Constants
-const appID = "app";
-const headingText = "To do. To done. ✅";
-
-// DOM Elements
-let appContainer = document.getElementById(appID);
-
-//
-// Functions
-//
-
-// Add a heading to the app container
-function inititialise() {
-  // If anything is wrong with the app container then end
-  if (!appContainer) {
-    console.error("Error: Could not find app contianer");
-    return;
-  }
-
-  // Create an h1 and add it to our app
-  const h1 = document.createElement("h1");
-  h1.innerText = headingText;
-  appContainer.appendChild(h1);
-
-  // Init complete
-  console.log("App successfully initialised");
-}
-
-//
-// Inits & Event Listeners
-//
-inititialise();
-*/
